944bd5b26219a54353a5bac5e7a5ac44a307053d
dudakovict <dudakovict@gmail.com>
2021-02-17 20:30:30 +0100

    fix

--- deploy.sh
+++ deploy.sh
@@ -3,31 +3,31 @@
 
 echo "Creating the volume..."
 
-kubectl apply -f ./kubernetes/postgres-persistent-volume.yaml
-kubectl apply -f ./kubernetes/postgres-persistent-volume-claim.yaml
+kubectl apply -f ./kubernetes/postgres/postgres-persistent-volume.yaml
+kubectl apply -f ./kubernetes/postgres/postgres-persistent-volume-claim.yaml
 
 
 echo "Creating the database credentials..."
 
-kubectl apply -f ./kubernetes/postgres-secret.yaml
+kubectl apply -f ./kubernetes/postgres/postgres-secret.yaml
 
 
 echo "Creating the postgres deployment and service..."
 
-kubectl create -f ./kubernetes/postgres-deployment.yaml
-kubectl create -f ./kubernetes/postgres-service.yaml
+kubectl create -f ./kubernetes/postgres/postgres-deployment.yaml
+kubectl create -f ./kubernetes/postgres/postgres-service.yaml
 
 
 echo "Creating twilio credentials..."
 
-kubectl apply -f ./kubernetes/twilio-secret.yaml
+kubectl apply -f ./kubernetes/auth/twilio-secret.yaml
 
 echo "Creating the flask deployment and service..."
 
-kubectl create -f ./kubernetes/auth-deployment.yaml
-kubectl create -f ./kubernetes/auth-service.yaml
+kubectl create -f ./kubernetes/auth/auth-deployment.yaml
+kubectl create -f ./kubernetes/auth/auth-service.yaml
 
 
 echo "Adding the ingress..."
 
-kubectl apply -f ./kubernetes/ingress-service.yaml
+kubectl apply -f ./kubernetes/ingress/ingress-service.yaml
